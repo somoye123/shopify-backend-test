a57194d60b00b34dca963b383d3e92e074991f63
somoye123 <somoye.ayotunde@gmail.com>
2021-01-12 10:34:24 +0100

    add validation for image field

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,8 +1,12 @@
 class Image < ApplicationRecord
   include Rails.application.routes.url_helpers
 
-    has_one_attached :image
+  has_one_attached :image
   
-    validates :name, :description, presence: true
+  validates :name, :description, :image, presence: true
+
+  def img_url
+    url_for(image)
+  end
   end
   
